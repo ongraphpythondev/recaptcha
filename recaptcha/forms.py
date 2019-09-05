b67b6118f31416203f856de5f27273512bbb0308
ongraphpythondev <pythondev@ongraph.com>
2019-09-05 13:05:23 +0530

    ReCaptchaV2 implemented

--- recaptcha/forms.py
+++ recaptcha/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV3
+
+# import for ReCaptchaV3
+# from captcha.widgets import ReCaptchaV3
 
 
 class FormWithCaptcha(forms.Form):
     first_name = forms.CharField(max_length=64)
     last_name = forms.CharField(max_length=64)
-    captcha = ReCaptchaField(widget=ReCaptchaV3)
+    captcha = ReCaptchaField()
+
+    # widget field for ReCaptchaV3
+    # captcha = ReCaptchaField(widget=ReCaptchaV3)
